43302c033d5de246f8e28b4f99f96553636f897c
Agrim Jain <agrim.jain@altudo.co>
2024-11-23 01:17:42 +0530

    new page

---
+++ components/auth-form.js
@@ -0,0 +1,26 @@
+"use client";
+
+export default function AuthForm({ mode }) {
+  return (
+    <form id="auth-form" action={formAction}>
+      <div>
+        <img src="/images/auth-icon.jpg" alt="A lock icon" />
+      </div>
+      <p>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+      </p>
+      <p>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </p>
+
+      <p>
+        <button type="submit">Create Account</button>
+      </p>
+      <p>
+        <Link href="/">Login with existing account.</Link>
+      </p>
+    </form>
+  );
+}
